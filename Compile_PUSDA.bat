4a5715af6d1ea5a2e78166dfb2b7f34294d8840a
username1565 <39200328+username1565@users.noreply.github.com>
2022-02-12 18:47:02 +0000

    Update and rename Compile_PUPSA.bat to Compile_PUSDA.bat

--- Compile_PUSDA.bat
+++ Compile_PUSDA.bat
@@ -1,6 +1,6 @@
 set fdir=%WINDIR%\Microsoft.NET\Framework
 set csc=%fdir%\v4.0.30319\csc.exe
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
-%csc% /out:PUPSA.exe /reference:System.Data.SQLite.dll PUPSA.cs
-PUPSA.exe
+%csc% /out:PUSDA.exe /reference:System.Data.SQLite.dll PUSDA.cs
+PUSDA.exe
 pause
