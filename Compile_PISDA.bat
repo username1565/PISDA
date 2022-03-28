e73a621287d33aaf74ba512faba679c8ca010f2a
username1565 <39200328+username1565@users.noreply.github.com>
2022-03-28 03:10:54 +0300

    Specify  /platform:x86 to use x86 "sqlite3.dll"
    Specify  /platform:x86 to use x86 "sqlite3.dll"

--- Compile_PISDA.bat
+++ Compile_PISDA.bat
@@ -1,6 +1,6 @@
 set fdir=%WINDIR%\Microsoft.NET\Framework
 set csc=%fdir%\v4.0.30319\csc.exe
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
-%csc% /out:PISDA.exe /reference:Mono.Data.Sqlite.dll PISDA.cs
+%csc% /out:PISDA.exe /reference:Mono.Data.Sqlite.dll /platform:x86 PISDA.cs
 PISDA.exe
 pause
